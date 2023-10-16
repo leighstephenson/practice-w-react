9c53bc6303b971031bd6c3f8d26b87058971b240
Leigh Stephenson <Leighh.stephenson@gmail.com>
2023-10-15 22:57:23 -0500

    Started working on saga to add and display notes

---
+++ src/redux/sagas/note.saga.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import { takeEvery, put } from 'redux-saga/effects';
+
+
+//! Add new note
+function* addNewNote(action) {
+    try {        
+        yield axios.post('/api/notes', action.payload);
+        yield put({ type: 'FETCH_NOTES' });
+    } catch (error) {
+        console.log(`Error in addNewNote in saga`, error);
+    }
+}
+
+//! Get all notes from the DB
+function* fetchAllNotes() {
+    try {
+        const notes = yield axios.get('/api/notes');
+        yield put({ type: 'SET_NOTES', payload: notes.data });
+    } catch (error) {
+        console.log('Error in fetchAllNotes in kit.saga');
+    }
+}
+
+function* noteSaga() {
+    yield takeEvery('FETCH_NOTES', fetchAllNotes);
+    yield takeEvery('ADD_NEW_NOTE', addNewNote)
+}; //End kitSaga()
+
+export default noteSaga;
