8a73f624ba9fe6657bb85031165b65f003e44518
Leigh Stephenson <Leighh.stephenson@gmail.com>
2023-10-16 21:09:41 -0500

    Added CSS animation to the user page, minor changes to the nav

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
-import {useSelector} from 'react-redux';
+import { useSelector } from 'react-redux';
+import { Typography } from '@mui/material';
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
   return (
-    <div className="container">
-      <h2>Welcome, {user.username}!</h2>
-      <p>Your ID is: {user.id}</p>
-      <LogOutButton className="btn" />
-    </div>
+    <center>
+      <div className="container">
+
+        <Typography variant="h5"> Welcome, {user.username}! </Typography>
+        <br />
+        <Typography variant="h3" className='openingMessage'> Let's get coding!</Typography>
+
+        <p>Your ID is: {user.id}</p>
+
+        <LogOutButton className="btn" />
+
+      </div>
+    </center>
   );
 }
 
