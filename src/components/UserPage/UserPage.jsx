555092741adabeeb2ca8f1089172447b4963d34c
Leigh Stephenson <Leighh.stephenson@gmail.com>
2023-10-31 18:46:33 -0500

    Working on how notes display

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -20,7 +20,7 @@ function UserPage() {
 
   //! Selects note
   const noteSelection = (note) => {
-    dispatch({ type: 'SET_SELECTED_NOTE', payload: note})
+    dispatch({ type: 'SET_SELECTED_NOTE', payload: note })
     console.log(`You selected the note with this title: ${note.notetitle}`);
   };
 
@@ -42,30 +42,38 @@ function UserPage() {
 
       </div>
 
-      <hr/>
+      <hr />
 
       <div className='notesList'>
         <Typography variant="h6"> Current notes: </Typography>
 
-        <br/> <br/>
-        
+        <br /> <br />
+
         {notes.map(note => {
           return (
             <Card key={note.id} className="noteCards"
-            onClick={() => noteSelection(note)} 
-            sx={{ marginBottom: 6, 
-              textAlign: 'center', 
-              boxShadow: 7, 
-              width: '95%',
-              maxWidth: 750, 
-              minWidth: 50}}
-              >
+              onClick={() => noteSelection(note)}
+              sx={{
+                marginBottom: 6,
+                textAlign: 'center',
+                boxShadow: 7,
+                width: '85%',
+                paddingTop: 3,
+                paddingBottom: 3,
+                maxWidth: 750,
+                minWidth: 50
+              }}
+            >
 
-              <h4> Title: {note.notetitle} </h4>
+              <h4> Title:  </h4>
+              <p>{note.notetitle} </p>
 
-              <p>Date Added: {note.dateadded} </p>
+              <h5>Date Added: </h5>
+              <p>{note.dateadded}  </p>
 
-              <p> Note: {note.notecontent}</p>
+              <p> Note: </p>
+              <p>{note.notecontent} </p>
+              
             </Card>
           );
         })
