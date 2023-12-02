cf66be82ecd4f477bab458284e606c805269a62a
Leigh Stephenson <Leighh.stephenson@gmail.com>
2023-12-01 21:10:24 -0600

    Started working on delete, added to note.saga

--- src/redux/sagas/note.saga.js
+++ src/redux/sagas/note.saga.js
@@ -21,9 +21,21 @@ function* fetchAllNotes() {
     }
 }
 
+//! Delete note
+//TODO finish hooking this up
+function* deleteNote(action) {
+    try {
+        yield axios.delete(`/api/note/${action.payload}`)
+        yield put ({ type: 'FETCH_KITS'});
+    } catch (error) {
+        console.log(`Error in deleteNote in saga ${error}`);
+    }
+}
+
 function* noteSaga() {
     yield takeEvery('FETCH_NOTES', fetchAllNotes);
     yield takeEvery('ADD_NEW_NOTE', addNewNote)
+    yield takeEvery ('DELETE_NOTE', deleteNote)
 }; //End kitSaga()
 
 export default noteSaga;
